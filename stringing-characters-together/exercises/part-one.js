8fbf43beadeb4d5df0bb9ab8071cffb21acf5b64
jminniemoore <JMINNIE.MOORE@GMAIL.COM>
2024-06-25 18:36:47 -0500

    string exercises 3 part

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,26 @@
-let num = 1001;
-
+let num = 10.01;
+let num2 = 12345
 //Returns 'undefined'.
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+if (String(num).includes(',')){
+    console.log(String(num).length-1)
+} else {
+    console.log(String(num).length)
+}
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+if (String(num2).includes('.')){
+    console.log(String(num2).length-1)
+} else {
+    console.log(String(num2).length)
+}
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (String(num2).includes('.')){
+    console.log(String(num2))
+} else {
+    console.log(String(num))
+} 
+if (String(num).indexOf(1001)){
+    console.log(String(num))
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,11 +3,18 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+console.log(language.slice(0,1)+language.slice(4,5))
 //2. Without using slice(), use method chaining to accomplish the same thing.
+let firstInitial = ('J');
+let secondInitial = ('S');
 
+console.log(firstInitial.concat(secondInitial))
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+let initials = ["JavaScript"]
+initialJ = ["J"]
+initialS = ["S"]
 
+console.log( `The abbreviation for '${language}' is '${initialJ + initialS}'`);
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
 //Part Three section Two
@@ -15,3 +22,4 @@ let language = 'JavaScript';
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+console.log(notTitleCase.toUpperCase)
