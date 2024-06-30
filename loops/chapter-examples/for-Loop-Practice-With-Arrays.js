80399f2a1acc98b01fcab890be2c01902d936ae3
jminniemoore <JMINNIE.MOORE@GMAIL.COM>
2024-06-30 12:50:20 -0500

    6.30 changes

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,13 @@
 // create an array variable containing the names
 
+let languages = ["JS", "Java", "C#", "Python"];
+let dogs = ["Posie", "Chico", "Buffy"]
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < languages.length; i++) {
+   console.log(languages[i]);
+}
+console.log(' ')
+for (let d = 0; d < dogs.length; d++) {
+    console.log(dogs[d])
+}
